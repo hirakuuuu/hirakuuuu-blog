73f8291e1a4a8ece1134c8688efe57feecf46ac9
hirakuuuu <m.hiraku1129@gmail.com>
2023-03-04 17:31:50 +0900

    [fix] 記事の一覧表示のレイアウトを修正

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,17 +1,17 @@
-import Avatar from './avatar'
-import DateFormatter from './date-formatter'
-import CoverImage from './cover-image'
-import Link from 'next/link'
-import type Author from '../interfaces/author'
+import Avatar from "./avatar";
+import DateFormatter from "./date-formatter";
+import CoverImage from "./cover-image";
+import Link from "next/link";
+import type Author from "../interfaces/author";
 
 type Props = {
-  title: string
-  coverImage: string
-  date: string
-  excerpt: string
-  author: Author
-  slug: string
-}
+  title: string;
+  coverImage: string;
+  date: string;
+  excerpt: string;
+  author: Author;
+  slug: string;
+};
 
 const PostPreview = ({
   title,
@@ -22,26 +22,29 @@ const PostPreview = ({
   slug,
 }: Props) => {
   return (
-    <div>
-      <div className="mb-5">
+    <div className="flex flex-row items-start">
+      {/* <div className="w-32 h-32">
         <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
-      <h3 className="text-3xl mb-3 leading-snug">
-        <Link
-          as={`/posts/${slug}`}
-          href="/posts/[slug]"
-          className="hover:underline"
-        >
-          {title}
-        </Link>
-      </h3>
-      <div className="text-lg mb-4">
-        <DateFormatter dateString={date} />
-      </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+      </div> */}
+      <Link
+        as={`/posts/${slug}`}
+        href="/posts/[slug]"
+        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <div className="flex-1 ml-2">
+          <h3 className="text-xl">{title}</h3>
+          <div className="my-5">
+            <p>{excerpt}</p>
+          </div>
+          <div className="flex flex-row">
+            <div className="font-bold text-xs my-auto">
+              <DateFormatter dateString={date} />
+            </div>
+          </div>
+        </div>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default PostPreview
+export default PostPreview;
